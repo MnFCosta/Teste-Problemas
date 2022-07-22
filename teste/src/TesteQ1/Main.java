21b910e6cf62dc719274130738cfe060c2e21469
Manoel Felipe Costa <mnfcosta1@gmail.com>
2022-07-22 10:38:15 -0300

    Update Main.java

--- teste/src/TesteQ1/Main.java
+++ teste/src/TesteQ1/Main.java
@@ -1,5 +1,19 @@
 package TesteQ1;
 
+// Observe o trecho de código abaixo:
+
+// int INDICE = 13, SOMA = 0, K = 0;
+
+// enquanto K < INDICE faça
+// {
+// K = K + 1;
+// SOMA = SOMA + K;
+// }
+
+// imprimir(SOMA);
+
+// Ao final do processamento, qual será o valor da variável SOMA?
+
 public class Main {
     public static void main(String[] args) {
         int indice = 13;
